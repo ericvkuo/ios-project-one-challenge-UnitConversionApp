5931a82e897b634f296fdf0a9e0b327884d741d9
Eric Kuo <111590422+ericvkuo@users.noreply.github.com>
2022-11-11 21:01:51 +0800

    Initial Commit

---
+++ UnitConversions/UnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConversionsApp.swift
+//  UnitConversions
+//
+//  Created by Eric Kuo on 2022/11/11.
+//
+
+import SwiftUI
+
+@main
+struct UnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
